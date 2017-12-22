23457f36b0c1fd76b659c09def1b1b1108eeb153
Thomas Volk <info@thomasvolk.de>
2017-12-22 16:04:19 +0100

    simple listener fuction

--- lib/registry.ex
+++ lib/registry.ex
@@ -11,24 +11,35 @@ defmodule Lighthouse.Registry do
     GenServer.call(__MODULE__, {:get_nodes, expiration_interval})
   end
 
+  def listen() do
+    GenServer.call(__MODULE__, {:listen_to})
+  end
+
   def register_node(ip) do
-    GenServer.call(__MODULE__, {:register_node, ip})
+    GenServer.cast(__MODULE__, {:register_node, ip})
   end
 
   def init(state) do
-    {:ok, {state} }
+    {:ok, {state, []} }
   end
 
   def filter_expired(nodes, expiration_interval) do
     nodes |> Enum.filter( fn {_k, v} -> DateTime.diff(DateTime.utc_now(), v, :milliseconds) <= expiration_interval end) |> Map.new
   end
 
-  def handle_call({:get_nodes, expiration_interval}, _form, {nodes} = state) do
+  def handle_call({:get_nodes, expiration_interval}, _form, {nodes, _listener} = state) do
     filtered_nodes = filter_expired(nodes, expiration_interval)
     {:reply, filtered_nodes, state}
   end
 
-  def handle_call({:register_node, ip}, _form, {nodes}) do
-    {:reply, :ok, { Map.put(nodes, ip, DateTime.utc_now()) } }
+  def handle_call({:listen_to}, {pid, _ref}, {nodes, listener}) do
+    new_listener = [ pid | listener ] |> Enum.uniq |> Enum.filter(&Process.alive?/1)
+    {:reply, :ok, { nodes, new_listener } }
+  end
+
+  def handle_cast({:register_node, ip}, {nodes, listener}) do
+    new_nodes = Map.put(nodes, ip, DateTime.utc_now())
+    listener |> Enum.map(fn pid ->  send(pid, {:update, new_nodes}) end)
+    {:noreply, { new_nodes, listener } }
   end
 end
--- test/registry_test.exs
+++ test/registry_test.exs
@@ -3,11 +3,21 @@ defmodule RegistryTest do
   doctest Lighthouse.Registry
 
   test "the Lighthouse.Registry should manage ip addresses" do
+    Lighthouse.Registry.listen()
     assert Lighthouse.Registry.get_nodes() == %{}
     Lighthouse.Registry.register_node({10, 1, 1, 50})
     %{ {10, 1, 1, 50} => _ } = Lighthouse.Registry.get_nodes()
     assert Lighthouse.Registry.get_nodes(0) == %{}
     Lighthouse.Registry.register_node({10, 1, 1, 51})
     %{ {10, 1, 1, 50} => _, {10, 1, 1, 51} => _ } = Lighthouse.Registry.get_nodes(300000)
+
+    result = receive do
+      {:update, %{ {10, 1, 1, 50} => _ }} ->
+        :ok
+    after
+      100 ->
+        assert false, "timeout! expected message not reached"
+    end
+    assert result == :ok
   end
 end
