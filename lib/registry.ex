3b828b2caf80bf78a6f93f0add353a53533f583e
Thomas Volk <info@thomasvolk.de>
2017-12-22 16:51:30 +0100

    unsubscribe/subscribe

--- lib/registry.ex
+++ lib/registry.ex
@@ -11,8 +11,12 @@ defmodule Lighthouse.Registry do
     GenServer.call(__MODULE__, {:get_nodes, expiration_interval})
   end
 
-  def listen() do
-    GenServer.call(__MODULE__, {:listen_to})
+  def subscribe() do
+    GenServer.call(__MODULE__, {:subscribe})
+  end
+
+  def unsubscribe() do
+    GenServer.call(__MODULE__, {:unsubscribe})
   end
 
   def register_node(ip) do
@@ -32,9 +36,14 @@ defmodule Lighthouse.Registry do
     {:reply, filtered_nodes, state}
   end
 
-  def handle_call({:listen_to}, {pid, _ref}, {nodes, listener}) do
+  def handle_call({:subscribe}, {pid, _ref}, {nodes, listener}) do
     new_listener = [ pid | listener ] |> Enum.uniq |> Enum.filter(&Process.alive?/1)
-    {:reply, :ok, { nodes, new_listener } }
+    {:reply, length(new_listener) > length(listener), { nodes, new_listener } }
+  end
+
+  def handle_call({:unsubscribe}, {in_pid, _ref}, {nodes, listener}) do
+    new_listener = listener |> Enum.filter(fn pid -> pid != in_pid end) |> Enum.filter(&Process.alive?/1)
+    {:reply, length(new_listener) < length(listener), { nodes, new_listener } }
   end
 
   def handle_cast({:register_node, ip}, {nodes, listener}) do
--- test/registry_test.exs
+++ test/registry_test.exs
@@ -3,10 +3,11 @@ defmodule RegistryTest do
   doctest Lighthouse.Registry
 
   test "the Lighthouse.Registry should manage ip addresses" do
-    Lighthouse.Registry.listen()
+    assert Lighthouse.Registry.subscribe()
     assert Lighthouse.Registry.get_nodes() == %{}
     Lighthouse.Registry.register_node({10, 1, 1, 50})
     %{ {10, 1, 1, 50} => _ } = Lighthouse.Registry.get_nodes()
+    assert Lighthouse.Registry.unsubscribe()
     assert Lighthouse.Registry.get_nodes(0) == %{}
     Lighthouse.Registry.register_node({10, 1, 1, 51})
     %{ {10, 1, 1, 50} => _, {10, 1, 1, 51} => _ } = Lighthouse.Registry.get_nodes(300000)
