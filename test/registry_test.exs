6c67f38073d4e0ea2d0521cd1bb33cbb17fb7dfb
Thomas Volk <info@thomasvolk.de>
2017-12-28 23:48:43 +0100

    fix tests

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -2,9 +2,16 @@ defmodule RegistryTest do
   use ExUnit.Case
   doctest Lighthouse.Registry
 
-  test "the Lighthouse.Registry should add nodes and remove expired nodes" do
+  setup do
     Lighthouse.Registry.purge()
-    assert Lighthouse.subscribe()
+    Lighthouse.subscribe()
+
+    on_exit fn ->
+     Lighthouse.unsubscribe()
+   end
+  end
+
+  test "the Lighthouse.Registry should add nodes and remove expired nodes" do
     assert Lighthouse.get_nodes() == []
     Lighthouse.Registry.register_node({10, 1, 1, 50}, "from::test")
     [ {{10, 1, 1, 50}, "from::test"} ] = Lighthouse.Registry.get_nodes()
@@ -13,7 +20,6 @@ defmodule RegistryTest do
     assert_receive {:lighthouse_nodes_updated, [ {{10, 1, 1, 50}, "from::test"} ] }
     assert_receive {:lighthouse_nodes_updated, [ ] }, 1000
 
-    assert Lighthouse.unsubscribe()
     Lighthouse.Registry.register_node({10, 1, 1, 51}, "from::test")
     [ {{10, 1, 1, 51}, "from::test"} ] = Lighthouse.Registry.get_nodes()
     Lighthouse.Registry.register_node({10, 1, 1, 52}, "from::test")
--- test/server_test.exs
+++ test/server_test.exs
@@ -2,10 +2,16 @@ defmodule UdpServerTest do
   use ExUnit.Case
   doctest Lighthouse.UdpServer
 
-  test "the Lighthouse.UdpServer should register nodes by receiving an udp event" do
-    assert Lighthouse.subscribe()
+  setup do
     Lighthouse.Registry.purge()
+    Lighthouse.subscribe()
 
+    on_exit fn ->
+     Lighthouse.unsubscribe()
+   end
+  end
+
+  test "the Lighthouse.UdpServer should register nodes by receiving an udp event" do
     pid = GenServer.whereis(Lighthouse.UdpServer)
     send pid, {:udp, :socket, {1,2,3,4}, 99999, 'test::udp::event'}
 
