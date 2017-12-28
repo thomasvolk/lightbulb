2cbf7439b0be90eeb20d776834cfeceb8de5edfb
Thomas Volk <info@thomasvolk.de>
2017-12-28 13:56:51 +0100

    refactor supervisor

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -5,30 +5,29 @@ defmodule Lighthouse.Supervisor do
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  defp add_server_worker(worker, udp_port) do
-    start_server = Application.get_env(:lighthouse, :server)
-    if start_server, do: worker ++ [ {Lighthouse.Ip4UdpServer, udp_port} ], else: worker
+  defp server_worker_spec(udp_port) do
+    {Lighthouse.Ip4UdpServer, udp_port}
   end
 
-  defp add_broadcast_worker(worker, udp_port) do
-    do_broadcast = Application.get_env(:lighthouse, :broadcast)
-    if do_broadcast do
-      interval = Application.get_env(:lighthouse, :broadcast_interval)
-      payload = Application.get_env(:lighthouse, :broadcast_message)
-      broadcast_address = Application.get_env(:lighthouse, :broadcast_address)
-      worker ++ [ {Lighthouse.Ip4UdpBroadcast, {udp_port, payload, broadcast_address, interval}} ]
-    else
-      worker
-    end
+  defp broadcast_worker_spec(udp_port) do
+    broadcast_interval = Application.get_env(:lighthouse, :broadcast_interval)
+    broadcast_message = Application.get_env(:lighthouse, :broadcast_message)
+    broadcast_address = Application.get_env(:lighthouse, :broadcast_address)
+
+    {Lighthouse.Ip4UdpBroadcast, {udp_port, broadcast_message, broadcast_address, broadcast_interval}}
   end
 
   def init(:ok) do
     udp_port = Application.get_env(:lighthouse, :udp_port)
     node_lifespan = Application.get_env(:lighthouse, :node_lifespan)
 
+    start_server = Application.get_env(:lighthouse, :server)
+    do_broadcast = Application.get_env(:lighthouse, :broadcast)
+
     worker = [{Lighthouse.Registry, {node_lifespan}} ]
-      |> add_server_worker(udp_port)
-      |> add_broadcast_worker(udp_port)
+    worker = if start_server, do: worker ++ [ server_worker_spec(udp_port) ], else: worker
+    worker = if do_broadcast, do: worker ++ [ broadcast_worker_spec(udp_port) ], else: worker
+
     Supervisor.init(worker, strategy: :one_for_one)
   end
 end
